e1e1a02fad9670f7e44b7b9b59b35466e2980168
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-04 22:16:25 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividad_10_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cree un diccionario de empleados y muestre un mensaje con el siguiente formato:\r\nID: ___ Nombre: ___");
+
+            for (int i = 0; i < 3; i++)
+            {
+
+
+                Console.WriteLine($"Ingrese  ID NO.{i + 1} de 3 productos");
+                int.TryParse(Console.ReadLine(), out clave);
+                if (producto.ContainsKey(clave))
+                {
+                    Console.WriteLine("valor ya ingresado ingrese otro");
+                    i--;
+
+                }
+                else
+                {
+                    Console.WriteLine("ingrese nombre:");
+                    nombre = Console.ReadLine();
+                    producto.Add(clave, nombre);
+
+                }
+
+            }
+            foreach (var item in producto)
+            {
+                Console.WriteLine("prodcuto Id: " + item.Key + "valor: " + item.Value);
+
+            }
+
+        }
+    }
+}
